4d12a6a2d67c4963695f7bc96cb21562f38defb6
DavidEBM <112018668+DavidEBM@users.noreply.github.com>
2023-02-26 11:20:12 -0500

    Add files via upload

---
+++ Bot Test 2/Bot Test 2/app.js
@@ -0,0 +1,40 @@
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+	{
+		name: 'ping',
+		description: 'Replies with Pong!',
+	},
+];
+
+const rest = new REST({ version: '10' }).setToken('MTA3ODgwMjI1Mzk2MTUxMDk0Mg.Gk3ztY.s_qxtu5aPkFEDIJiOLfonraLOSo66AK9f88f4Y');
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(Routes.applicationCommands(1078802253961510942), { body: commands });
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+
+client.login('MTA3ODgwMjI1Mzk2MTUxMDk0Mg.Gk3ztY.s_qxtu5aPkFEDIJiOLfonraLOSo66AK9f88f4Y');
